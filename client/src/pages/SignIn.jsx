54f2e52b3453da3d7c4590312cbb36ddea013a68
Manoharchow9848 <=manuchow1.767@gmail.com>
2024-08-11 21:19:59 +0530

    Complete signin page functionality - MERN Blog

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,7 +3,32 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
 const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        
+        navigate('/');
+      }
+    } catch (error) {
+      
+    }
+  }
   return (
+    
     <div className='min-h-screen mt-20'>
     <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
       {/* left */}
@@ -22,14 +47,14 @@ const SignIn = () => {
       {/* right */}
 
       <div className='flex-1'>
-        <form className='flex flex-col gap-4' >
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit} >
           <div>
             <Label value='Your email' />
             <TextInput
               type='email'
               placeholder='name@company.com'
               id='email'
-              
+              onChange={handleChange}
             />
           </div>
           <div>
@@ -38,16 +63,23 @@ const SignIn = () => {
               type='password'
               placeholder='**********'
               id='password'
-             
+              onChange={handleChange}
             />
           </div>
           <Button
-            gradientDuoTone='purpleToPink'
-            type='submit'
-            
-          >
-           Sign-in
-          </Button>
+              gradientDuoTone='purpleToPink'
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign In'
+              )}
+            </Button>
           <OAuth />
         </form>
         <div className='flex gap-2 text-sm mt-5'>
