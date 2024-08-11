91a1d4c912b275ea4dc1eb0ba456d1e0c2e2fb5f
Manoharchow9848 <=manuchow1.767@gmail.com>
2024-08-11 20:23:31 +0530

    Add functionality to the sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,6 +4,38 @@ import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });  
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);      
+    }
+  }
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -23,14 +55,14 @@ const SignUp = () => {
         {/* right */}
 
         <div className='flex-1'>
-          <form className='flex flex-col gap-4' >
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your username' />
               <TextInput
                 type='text'
                 placeholder='Username'
                 id='username'
-               
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -39,7 +71,7 @@ const SignUp = () => {
                 type='email'
                 placeholder='name@company.com'
                 id='email'
-           
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -48,15 +80,22 @@ const SignUp = () => {
                 type='password'
                 placeholder='Password'
                 id='password'
-                
+                onChange={handleChange}
               />
             </div>
             <Button
               gradientDuoTone='purpleToPink'
               type='submit'
-             
+              disabled={loading}
             >
-              Sign-up
+            {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
             </Button>
             <OAuth />
          
@@ -67,7 +106,11 @@ const SignUp = () => {
               Sign In
             </Link>
           </div>
-          
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )} 
         </div>
       </div>
     </div>
