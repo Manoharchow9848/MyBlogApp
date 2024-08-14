806a8b79cfd327eb10f22395e132fb2a50a6c94d
Manoharchow9848 <=manuchow1.767@gmail.com>
2024-08-14 20:30:53 +0530

    Update header component with user data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,7 @@ export default function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/projects' element={<Projects />} />
+        <Route path='/dashboard' element={<DashBoard />} />
       </Routes>
       <Footer />
       </BrowserRouter>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import { Avatar, Button, Dropdown ,Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
-const Header = () => {
-    const path = useLocation().pathname;
+import { useSelector, useDispatch } from 'react-redux';
+
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useEffect, useState } from 'react';
+
+export default function Header() {
+  const path = useLocation().pathname;
   const location = useLocation();
   const navigate = useNavigate();
-    const theme="dark";
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const theme = "light"
+  const [searchTerm, setSearchTerm] = useState('');
+
+  
+
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
-  <Navbar className='border-b-2'>
-     <Link
+    <Navbar className='border-b-2'>
+      <Link
         to='/'
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
@@ -19,35 +39,57 @@ const Header = () => {
         </span>
         Blog
       </Link>
-      <form >
+      <form onSubmit={handleSubmit}>
         <TextInput
           type='text'
           placeholder='Search...'
           rightIcon={AiOutlineSearch}
           className='hidden lg:inline'
-         
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-        <AiOutlineSearch />
-      </Button>
+      
       <div className='flex gap-2 md:order-2'>
-      <Button  className='w-12 h-10 hidden sm:inline'
+        <Button
+          className='w-12 h-10 hidden sm:inline'
           color='gray'
           pill
-      >
-       {theme === 'light' ? <FaSun /> : <FaMoon />}
-      </Button>
-      <Link to='/sign-in'>
+         
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
+        </Button>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar img={currentUser.profilePicture} alt='user' rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item >Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
             <Button gradientDuoTone='purpleToBlue' outline>
               Sign In
             </Button>
           </Link>
-          <Navbar.Toggle />
-        
+        )}
+        <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-          <Navbar.Link active={path === '/'} as={'div'}>
+        <Navbar.Link active={path === '/'} as={'div'}>
           <Link to='/'>Home</Link>
         </Navbar.Link>
         <Navbar.Link active={path === '/about'} as={'div'}>
@@ -56,9 +98,7 @@ const Header = () => {
         <Navbar.Link active={path === '/projects'} as={'div'}>
           <Link to='/projects'>Projects</Link>
         </Navbar.Link>
-          </Navbar.Collapse>
-  </Navbar>
-  )
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
-
-export default Header
