be86e987454ac156c251b00afae87a377f2a66a6
Manoharchow9848 <=manuchow1.767@gmail.com>
2024-08-15 15:13:51 +0530

    Add signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -135,6 +135,21 @@ const DashProfile = () => {
       dispatch(deleteUserFailure(error.message));
     }
   };
+  const handleSignout = async()=>{
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
@@ -229,7 +244,7 @@ const DashProfile = () => {
       <span onClick={() => setShowModal(true)} className='cursor-pointer'>
         Delete Account
       </span>
-      <span  className='cursor-pointer'>
+      <span  className='cursor-pointer' onClick={handleSignout}>
         Sign Out
       </span>
     </div>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,6 +27,21 @@ export default function Header() {
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
+  const handleSignout = async()=>{
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <Navbar className='border-b-2'>
@@ -77,7 +92,7 @@ export default function Header() {
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
-            <Dropdown.Item >Sign out</Dropdown.Item>
+            <Dropdown.Item onClick={handleSignout} >Sign out</Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to='/sign-in'>
