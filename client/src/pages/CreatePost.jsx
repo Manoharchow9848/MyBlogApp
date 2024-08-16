e275541521f5a79e57509fb815cf9f6002e8398c
Manoharchow9848 <=manuchow1.767@gmail.com>
2024-08-16 23:10:35 +0530

    Complete upload post image functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -81,6 +81,7 @@ export default function CreatePost() {
     } catch (error) {
       setPublishError('Something went wrong');
     }
+    
   };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
