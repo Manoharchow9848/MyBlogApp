c6b3668c8244d0ab650b92eb378223b5aed244ff
Manoharchow9848 <=manuchow1.767@gmail.com>
2024-08-18 20:05:31 +0530

    changes added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -22,7 +22,7 @@ export default function FooterCom() {
               <Footer.Title title='About' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href=''
+                  href='https://chatterzone-mc.onrender.com/'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
