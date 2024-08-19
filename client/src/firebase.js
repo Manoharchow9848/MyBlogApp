cf6714e1fee8394ec69a1287664301b950dd54dd
Manoharchow9848 <=manuchow1.767@gmail.com>
2024-08-19 09:35:01 +0530

    .env updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBP6kyQl7IVbkKjYdqsul1txeMRPowcgqI",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "myblogapp-27468.firebaseapp.com",
   projectId: "myblogapp-27468",
   storageBucket: "myblogapp-27468.appspot.com",
